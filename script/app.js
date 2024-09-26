751ff194234f22ed0bfe7dd407f5ade846e3f682
mdjonayed6 <jonayed2640@gmail.com>
2024-09-27 01:05:44 +0600

    coupon code applied

--- script/app.js
+++ script/app.js
@@ -29,8 +29,32 @@ for (const btn of allBtn) {
 
     selectedContainer.appendChild(div);
     updateTotalTicketPrice(result);
+    updatedGrandTotalTicketPrice();
     updatedSeatCount();
+    updatedLeftSeatCount();
   });
+}
+
+// updated grand total amount
+function updatedGrandTotalTicketPrice(status) {
+  const totalCost = getTargetedValue("total-price"); //grand total calculation here
+  if (status == undefined) {
+    //coupon code apply start here
+    document.getElementById("grand-total").innerText = totalCost;
+  } else {
+    const couponCode = document.getElementById("coupon-code").value;
+    if (couponCode == "NEW15") {
+      const discountAmount = totalCost * 0.15; //15% discount
+      document.getElementById("grand-total").innerText =
+        totalCost - discountAmount;
+    } else if (couponCode == "Couple20") {
+      const discountAmount = totalCost * 0.2; //20% discount
+      document.getElementById("grand-total").innerText =
+        totalCost - discountAmount;
+    } else {
+      alert("Please Enter Valid Coupon Code"); // coupon code apply ends here
+    }
+  }
 }
 
 //updated total price
@@ -40,13 +64,19 @@ function updateTotalTicketPrice(value) {
   document.getElementById("total-price").innerText = sum;
 }
 
-// updated seat 
-function updatedSeatCount (value){
+// updated seat
+function updatedSeatCount(value) {
   const seatCount = getTargetedValue("selected-seat");
   const update = seatCount + 1;
   document.getElementById("selected-seat").innerText = update;
 }
 
+// Left seat updated
+function updatedLeftSeatCount(value) {
+  const leftSeatCount = getTargetedValue("seat-left");
+  const updateLeftSeat = leftSeatCount - 1;
+  document.getElementById("seat-left").innerText = updateLeftSeat;
+}
 
 function getTargetedValue(id) {
   const target = document.getElementById(id).innerText;
